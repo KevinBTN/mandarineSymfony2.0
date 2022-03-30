6fd714caa44f27669b2306d99daec6357fcfb4bf
QuentinBOul <100684130+QuentinBOul@users.noreply.github.com>
2022-03-30 16:11:21 +0200

    Modif mineure Search
    - label "emplacement" remplacé par un placeholder "Où voulez-vous aller?"

--- src/Form/PropertySearchType.php
+++ src/Form/PropertySearchType.php
@@ -6,13 +6,20 @@ use App\Entity\PropertySearch;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class PropertySearchType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('emplacement')
+            ->add('emplacement', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Où voulez-vous aller ?'
+                ]
+            ])
         ;
     }
 
